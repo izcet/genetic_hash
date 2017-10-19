44a98dc9dcb646b9da37b3930b118301db3e0d51
izcet <isaacjrhett@gmail.com>
2017-10-19 14:11:53 -0700

    [string_gen/string_gen] [string_gen/randA.txt] [string_gen/rand40.txt] [string_gen/main.go] [string_gen/global_list.txt] [string_gen/funcs.go] +[string_gen/randRandAB.txt] +[string_gen/fooBarBaz.txt] +[string_gen/fizzBuzz.txt]

--- string_gen/funcs.go
+++ string_gen/funcs.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"math/rand"
 	"bytes"
+	"strconv"
 )
 
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -17,17 +18,80 @@ func	rand40() string {
 	return (buffer.String())
 }
 
-var fun = repeat()
+var lotsA = repeatA()
 
 func	randA() string {
-	str := fun("a")
+	str := lotsA("a")
 	return (str)
 }
 
-func	repeat() func(string) string {
+func	repeatA() func(string) string {
 	strLen := 0
 	return func(str string) string {
 		strLen += 1
 		return (strings.Repeat(str, strLen))
 	}
 }
+
+func	randRandAB() string {
+	var buffer bytes.Buffer
+	len := rand.Intn(80)
+	for i := 0; i <= len; i++ {
+		buffer.WriteString(string(letters[rand.Intn(2)]))
+	}
+	return (buffer.String())
+}
+
+func	fooBarBaz() string {
+	var buffer bytes.Buffer
+	len := rand.Intn(20)
+	for i := 0; i <= len; i++ {
+		x := rand.Intn(4)
+		var str string
+		switch x {
+		case 0: 
+			str = "foo"
+		case 1:
+			str = "bar"
+		case 2:
+			str = "baz"
+		case 3:
+			str = " "
+		}
+		buffer.WriteString(str)
+	}
+	return (buffer.String())
+}
+
+var lotsFizz = repeatFizz()
+
+func	fizzBuzz() string {
+	str := lotsFizz()
+	return (str)
+}
+
+func	repeatFizz() func() string {
+	strLen := 0
+	var buffer bytes.Buffer
+	return func() string {
+		strLen += 1
+		if (strLen != 1) {
+			buffer.WriteString(", ")
+		}
+		fb := 0
+		if (strLen % 3) == 0 {
+			buffer.WriteString("fizz")
+			fb += 1
+		}
+		if (strLen % 5) == 0 {
+			buffer.WriteString("buzz")
+			fb += 1
+		}
+		if (fb == 0) {
+			buffer.WriteString(strconv.Itoa(strLen))
+		}
+		result := buffer.String() + "\n"
+		return (result)
+	}
+}
+
--- string_gen/main.go
+++ string_gen/main.go
@@ -15,7 +15,7 @@ func	setFile(filename string) (*os.File, error) {
 
 func	main() {
 	global := "global_list.txt"
-	numStrings := 60
+	numStrings := 256
 	
 	file, err := setFile(global)
 	if (err != nil) {
@@ -27,6 +27,9 @@ func	main() {
 
 	makeFileList("40 random characters", "rand40.txt", rand40, file, numStrings)
 	makeFileList("increasing numbers of 'a'", "randA.txt", randA, file, numStrings)
+	makeFileList("random combinations of 'a' and 'b' of random lengths", "randRandAB.txt", randRandAB, file, numStrings)
+	makeFileList("foos, bars, and bazs", "fooBarBaz.txt", fooBarBaz, file, numStrings)
+	makeFileList("fizzbuzz iterations", "fizzBuzz.txt", fizzBuzz, file, numStrings)
 }
 
 func	makeFileList(description, filename string, function func() string, global *os.File, numStrings int) {
@@ -62,7 +65,7 @@ func	getList(function func() string, numStrings int) map[string]int {
 	list := make(map[string]int)
 	for ; len(list) < numStrings; {
 		str := function()
-		fmt.Println(str)
+		//fmt.Println(str) // debugging individual strings
 		if _, ok := list[str]; ok == false {
 			list[str] = 0
 		}
